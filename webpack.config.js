8724e10da54c3ccd5eab726883b82455d133a2fd
Wales <Awales@Cranel.com>
2018-09-25 12:09:42 -0400

    initial front end app

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,41 @@
-ECHO is on.
+'use strict'
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLPlugin = require('html-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+module.exports = {
+  devtool: 'eval',
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    filename: 'bundle-[hash].js',
+    path: `${__dirname}/build`,
+    publicPath: '/',
+  },
+  plugins: [
+    new HTMLPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+}
